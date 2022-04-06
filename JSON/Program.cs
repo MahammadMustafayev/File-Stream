d7cbde826a91e5da3909ada2ed315f7bf54c8282
Mahammad Mustafayev <tu7ekdm78@aztu.local>
2022-04-06 13:39:06 +0400

    modified by me

--- JSON/Program.cs
+++ JSON/Program.cs
@@ -15,9 +15,13 @@ namespace JSON
             Product headphones = new Product { Id = 2, Name = "JBL Giannis", Price = 350 };
             Product ssd = new Product { Id = 3, Name = "A400 SATA SSD", Price = 55 };
             OrderItem item1 = new OrderItem { Id = 1, Product = laptop, Count = 1, TotalPrice = laptop.Price * 1 };
+            item1.TotalPrice = laptop.Price * item1.Count;
             OrderItem item2 = new OrderItem { Id = 2, Product = ssd, Count = 4, TotalPrice = ssd.Price * 4 };
+            item2.TotalPrice = headphones.Price * item2.Count;
             OrderItem item3 = new OrderItem { Id = 3, Product = headphones, Count = 2, TotalPrice = headphones.Price * 2 };
+            item3.TotalPrice = ssd.Price * item3.Count;
             OrderItem item4 = new OrderItem { Id = 4, Product = laptop, Count = 1, TotalPrice = laptop.Price * 1 };
+            item4.TotalPrice = laptop.Price * item4.Count;
             List<OrderItem> orderItems1 = new List<OrderItem>();
             orderItems1.Add(item1);
             orderItems1.Add(item4);
@@ -27,22 +31,22 @@ namespace JSON
             Order order1 = new Order { Id = 1, orderItems = orderItems1 };
             Order order2 = new Order { Id = 1, orderItems = orderItems2 };
             var jsonObj = JsonConvert.SerializeObject(order1);
-            using (StreamWriter sw = new StreamWriter(@"C:\Users\admin\OneDrive\İş masası\JSON\JSON\Files\json1.json"))
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\tu7ekdm78\Desktop\File-Stream\JSON\Files\json2.json"))
             {
                 sw.WriteLine(jsonObj);
             }
             #endregion
             #region DeserializeObject
-            string temp;
-            using (StreamReader sr = new StreamReader(@"C:\Users\admin\OneDrive\İş masası\JSON\JSON\Files\json1.json"))
-            {
-                temp = sr.ReadToEnd();
-            }
-            Order order = JsonConvert.DeserializeObject<Order>(temp);
-            Console.WriteLine(order.orderItems[0].Product.Id);
-            Console.WriteLine(order.orderItems[0].Product.Name);
-            Console.WriteLine(order.orderItems[1].Product.Id);
-            Console.WriteLine(order.orderItems[1].Product.Name);
+            //string temp;
+            //using (StreamReader sr = new StreamReader(@"C:\Users\tu7ekdm78\Desktop\File-Stream\JSON\Files\json1.json"))
+            //{
+            //    temp = sr.ReadToEnd();
+            //}
+            //Order order = JsonConvert.DeserializeObject<Order>(temp);
+            //Console.WriteLine(order.orderItems[0].Product.Id);
+            //Console.WriteLine(order.orderItems[0].Product.Name);
+            //Console.WriteLine(order.orderItems[1].Product.Id);
+            //Console.WriteLine(order.orderItems[1].Product.Name);
             #endregion
         }
     }
